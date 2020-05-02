a5e47e0d9067fa43f0c0f637096a9fa38efff983
Indraneel Srivastava <indraneel.max@gmail.com>
2020-05-02 18:01:06 +0100

    fix url endpoint

--- django_react_proj_test/urls.py
+++ django_react_proj_test/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^api/students/$', views.students_list),
-    re_path(r'^api/students/(?P[0-9]+)$', views.students_detail),
+    re_path(r'^api/students/([0-9]+)$', views.students_detail),
 ]
